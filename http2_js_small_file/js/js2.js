20ac7e977489b0205ae98216ff1fa0db16a067a1
smohan24 <talk2sreejamohan@gmail.com>
2017-05-10 18:07:09 -0400

    FCN Data Setup

---
+++ http2_js_small_file/js/js2.js
@@ -0,0 +1,1 @@
+(function(){console.log("small obj test");})();
---
+++ quic_js_small_files/www.example.org/js/js20.js
@@ -0,0 +1,16 @@
+HTTP/1.1 200 OK
+X-Original-Url: https://www.example.org/js/js20.js
+Vary: Accept-Encoding, Origin
+Content-Type: text/javascript
+Date: Wed, 08 Mar 2017 14:50:31 GMT
+Expires: Thu, 08 Mar 2018 14:50:31 GMT
+Last-Modified: Thu, 21 Apr 2016 03:17:22 GMT
+X-Content-Type-Options: nosniff
+Server: sffe
+X-XSS-Protection: 1; mode=block
+Cache-Control: public, max-age=31536000
+Age: 5360061
+Alt-Svc: quic=":443"; ma=2592000; v="37,36,35"
+Accept-Ranges: none
+
+(function(){console.log("small obj test");})();
